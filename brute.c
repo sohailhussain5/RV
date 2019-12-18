e4d22c58258e6cc1cd0dbdc99d2354e99334bf15
Sohail <sohailhussain5@email.com>
2019-12-19 02:26:43 +0530

    initial import

---
+++ brute.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <unistd.h>
+
+char t[100], p[50];
+int brute_force()
+{
+    int n, j, m, i;
+    n = strlen(t);
+    m = strlen(p);
+
+    for (i = 0; i < n; i++)
+    {
+        j = 0;
+        while (j < m && t[i + j] == p[j])
+        {
+            j++;
+            if (j == m)
+                return i + 1; //pattern found
+        }
+    }
+    return -1; //pattern not found
+}
+
+void main()
+{
+    struct timeval start, end;
+    int pos;
+    FILE *out_file = fopen("stats.txt", "a"); // append only
+    printf("Enter the Source String :-  ");
+    gets(t);
+    printf("Enter the pattern :-  ");
+    gets(p);
+
+    mingw_gettimeofday(&start, NULL);
+    pos = brute_force();
+    // sleep(1);
+    mingw_gettimeofday(&end, NULL);
+
+    if (pos == -1) 
+        printf("%s pattern not found in text", p);
+    else
+        printf("%s pattern found at index %d", p, pos);
+
+    
+    long seconds = (end.tv_sec - start.tv_sec);
+    long micros = (end.tv_usec - start.tv_usec);
+
+    printf("\nTime elapsed is %d seconds and %d micros\n", seconds, micros);
+
+
+    if (out_file == NULL)
+    {
+        printf("Error! Could not open file\n");
+        exit(-1); 
+    }
+    fprintf(out_file, "\nTime elapsed is %d seconds and %d micros\n", seconds, micros); 
+
+
+}
